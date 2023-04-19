b5e3535a8cee6b1b3f89a9dd77024e5b22d7589f
FaridFarhutdinov <128831029+FaridFarhutdinov@users.noreply.github.com>
2023-04-19 13:57:44 +0500

    added new enemy

--- Assets/Scenes/level2/scripts/FlyingEnemyMove.cs
+++ Assets/Scenes/level2/scripts/FlyingEnemyMove.cs
@@ -17,23 +17,17 @@ public class FlyingEnemyMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //GetComponent<Rigidbody2D>().velocity = new Vector2(speed * direction, GetComponent<Rigidbody2D>().velocity.y);
+        //GetComponent<Rigidbody2D>().velocity = new Vector2(speed * directionX, GetComponent<Rigidbody2D>().velocity.y);
         transform.position = transform.position + new Vector3(directionX * speed * Time.deltaTime, directionY * speed * Time.deltaTime, 0);
         transform.localScale = new Vector3(directionX, 1, 1);
-    }
-
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.tag == "level")
-            directionX *= -1f;
     }
 
     void FixedUpdate()
     {
         PathLength += 1;
-        if (PathLength == 450){
+        if (PathLength == 300){
             directionX *= -1f;
-            PathLength = 0;
+            PathLength = 1;
         }
 
         if (PathLength % 30 == 0){
