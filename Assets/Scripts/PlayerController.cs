12d29dc674253a665e7f7c29eb30869c992fc729
duckonspoon <114981314+duckonspoon@users.noreply.github.com>
2023-04-27 23:41:24 +0500

    add attack input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -144,15 +144,16 @@ public class PlayerController : MonoBehaviour
     {
         if(context.started && touchingDirections.IsGrounded)
         {
-            animator.SetTrigger(AnimationStrings.jump);
+            animator.SetTrigger(AnimationStrings.jumpTrigger);
             rb.velocity = new Vector2(rb.velocity.x, jumpImpulse);
         }
     }
 
-    public void MakeAHit(InputAction.CallbackContext context)
+    public void OnAttack(InputAction.CallbackContext context)
     {
         if (context.started)
         {
+            animator.SetTrigger(AnimationStrings.attackTrigger);
             Instantiate(Hit, transform.position, Quaternion.identity);
             Destroy(Hit);
         }
