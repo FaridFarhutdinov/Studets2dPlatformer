f5e844239957b00198b32acd20f0a506c2b528e4
FaridFarhutdinov <128831029+FaridFarhutdinov@users.noreply.github.com>
2023-05-26 22:49:14 +0500

    maybe last updates

--- Assets/Scripts/Hog.cs
+++ Assets/Scripts/Hog.cs
@@ -5,28 +5,59 @@ using UnityEngine;
 public class Hog : MonoBehaviour
 {
     public float walkSpeed  = 3f;
-
+    private int direction = -1;
+    private bool Is_Stuned = false;
+    public int BossHp = 30;
     Rigidbody2D rb;
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(walkSpeed * Vector2.right.x, rb.velocity.y);
-    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Is_Stuned == false)
+        {
+            rb.velocity = new Vector2(walkSpeed * direction, rb.velocity.y);
+        }
+
+        if (BossHp < 1) Death();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (Is_Stuned == false)
+        {
+            rb.velocity = new Vector2(walkSpeed * direction * 5, rb.velocity.y);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "walls")
+        {
+            Is_Stuned = true;
+            rb.AddForce(new Vector2(-15 * transform.localScale.x, 5) * 10f, ForceMode2D.Force);
+            Invoke("Prosnuca", 3f);
+        }
+
+        if (col.gameObject.tag == "hit")
+        {
+            BossHp--;
+        }
+    }
+
+    void Prosnuca()
+    {
+        direction *= -1;
+        transform.localScale = new Vector3(direction, 1, 1);
+        Is_Stuned = false;
+    }
+
+    void Death()
+    {
+        Destroy(this.gameObject);
     }
 }
